01797eafe4ac087735e40fb9fed4f989ab3c46eb
GenericJam <GenericJam@gmail.com>
2026-04-11 00:37:35 -0600

    Add docs

--- mix.exs
+++ mix.exs
@@ -16,10 +16,12 @@ defmodule MobDev.MixProject do
 
   defp deps do
     [
-      {:eqrcode,   "~> 0.2"},
-      {:jason,     "~> 1.4"},
-      {:avatarex,  git: "https://github.com/GenericJam/avatarex.git", ref: "deedd09", only: [:dev, :test]},
-      {:image,     "~> 0.54", only: [:dev, :test]}
+      {:eqrcode, "~> 0.2"},
+      {:jason, "~> 1.4"},
+      {:avatarex,
+       git: "https://github.com/GenericJam/avatarex.git", ref: "deedd09", only: [:dev, :test]},
+      {:image, "~> 0.54", only: [:dev, :test]},
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 end
