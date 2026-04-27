57925af04a768c480444506a4a7ef7c24409aa6c
GenericJam <GenericJam@gmail.com>
2026-04-27 16:20:49 -0600

    0.3.10

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MobDev.MixProject do
   def project do
     [
       app: :mob_dev,
-      version: "0.3.9",
+      version: "0.3.10",
       elixir: "~> 1.19",
       description: "Development tooling for the Mob mobile framework",
       source_url: "https://github.com/genericjam/mob_dev",
